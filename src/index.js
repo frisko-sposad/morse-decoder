d51a94ba881cfb4be2acc6582a7a2c7a94f7c02b
Алексей Ренатович Сафин <60577397+frisko-sposad@users.noreply.github.com>
2020-03-18 17:29:05 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -35,10 +35,41 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/.{1,10}/g);
+    let arr2 = [];
+    let str = "";
+    let arr3 = [];
+    let res = "";
+    
+    
+    
+    for (let i = 0; i<arr.length; i++) {
+        if (arr[i] == "**********") { arr[i] = " ";}
+      str  = arr[i];
+     
+          
+      arr2 = str.match(/.{1,2}/g);   
+        
+        for (let i = 0; i<arr2.length; i++) {
+          if (arr2[i] == "00") { arr2[i] = ""; }
+          if (arr2[i] == "10") { arr2[i] = "."; }
+          if (arr2[i] == "11") { arr2[i] = "-"; }  
+          if (arr2[i] == " ") { arr2[i] = " "; }
+        }
+        arr3.push(arr2.join(''));     
+    }
+     
+     for (let i = 0; i<arr3.length; i++) {
+    
+     
+     res = res + MORSE_TABLE[arr3[i]];
+      
+    }
+return res;
 }
 
 module.exports = {
